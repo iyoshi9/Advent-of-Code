1d6363d28a826c545ec95d6cea621deafdaf34f8
iyoshi9 <sebastiangio@live.dk>
2023-12-01 20:00:52 +0100

    Update day1.py

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,14 +1,6 @@
 import re
-
-list_of_lines = open('Day_1.txt', 'r')
-list_of_lines = list_of_lines.readlines()
-pattern = re.compile('[abcdefghijklmnopqrstuvwxyz]')
-data_list = []
-data_list2 = []
-for entry in list_of_lines:
-    line_cleaned = entry.strip().split()
-    if len(line_cleaned) == 0 or line_cleaned[0] == '%':
-        continue
-    number = pattern.split(entry)
-    data_list.append(number)
-print(data_list)
+file = open("Day_1.txt",'r')
+pattern = re.compile('[a-z]')
+for line in file.readlines():
+    if pattern.match(line):
+        print(line)
