58413534b2ae2f7eb01b7f8b2d0b272eeb876802
iyoshi9 <sebastiangio@live.dk>
2023-12-01 15:41:09 +0100

    day1

---
+++ 2023/day1.py
@@ -0,0 +1,12 @@
+
+
+list_of_lines = open('day1.txt', 'r')
+list_of_lines = list_of_lines.readlines()
+data_list = []
+for entry in list_of_lines:
+    line_cleaned = entry.strip().split()
+    if len(line_cleaned) == 0 or line_cleaned[0] == '%':
+        continue
+    entry.translate({ord(i): None for i in 'abcdefghijklmnopqrstuvxyz'})
+    data_list.append(entry)
+print(data_list)
