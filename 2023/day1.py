e6f1e4b28ed18f50fdaee244f27085d70c0acfc0
iyoshi9 <sebastiangio@live.dk>
2023-12-01 16:38:03 +0100

    Update day1.py

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,12 +1,14 @@
-
+import re
 
 list_of_lines = open('Day_1.txt', 'r')
 list_of_lines = list_of_lines.readlines()
+pattern = re.compile('[abcdefghijklmnopqrstuvwxyz]')
 data_list = []
+data_list2 = []
 for entry in list_of_lines:
     line_cleaned = entry.strip().split()
     if len(line_cleaned) == 0 or line_cleaned[0] == '%':
         continue
-    entry.translate({ord(i): None for i in 'abcdefghijklmnopqrstuvxyz'})
-    data_list.append(entry)
+    number = pattern.split(entry)
+    data_list.append(number)
 print(data_list)
