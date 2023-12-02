1b3c607a0e27dae27cf62fb606b0eaf6b8809619
iyoshi9 <sebastiangio@live.dk>
2023-12-02 16:30:57 +0100

    Update day2.py

--- 2023/day2.py
+++ 2023/day2.py
@@ -4,8 +4,8 @@
 file = open("Day_2.txt",'r')
 sum = 0
 for entry in file:
-    line = entry.strip().split()
-    if "red" <= 12 and "green" <= 13 and "blue" <= 14 :
+    line = entry.strip().split() # Not sure if this is necessary
+    if "red" <= 12 and "green" <= 13 and "blue" <= 14 : # Need to get the value of the colors from every line
         continue
-    sum = sum + int(line[1])
+    sum = sum + int(line[1]) # Need to get the colon symbol away from the number gracefully
 print(sum)
