63ec4a7eda0dd88b445effb6ce497047ef662487
iyoshi9 <sebastiangio@live.dk>
2023-12-03 16:20:32 +0100

    day3

---
+++ 2023/day3.py
@@ -0,0 +1,9 @@
+
+file = open("Day_3.txt",'r')
+sum = 0
+
+for entry in file:
+    line = entry.strip().split() # Not sure if necessary
+    # if number is adjacent to a symbol (that is not .) then put it in sum
+    sum = sum + "number"
+print(sum)
