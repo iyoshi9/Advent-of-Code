567a780cb6a57423be81af41e4aee147a133b5c9
iyoshi9 <sebastiangio@live.dk>
2023-12-05 19:23:00 +0100

    Update day1.py

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,12 +1,18 @@
 import re
 
 # Part 1
-file = open("Day_1.txt",'r')
-pattern = re.compile('[a-z]')
-for line in file.readlines():
-    if pattern.match(line):
-        sub = pattern.sub("",line)
-        print(sub)           
-for line in sub:
-    pass
+# Two ideas that both not really working
+list_of_lines = open('Day_1.txt', 'r')
+#pattern = re.compile('[a-z]')
+#data_list = []
+#for entry in list_of_lines:
+#    number = pattern.split(entry)
+#    data_list.append(number)
+#print(data_list)    
+pattern = re.compile(".*[0-9]")
+for entry in list_of_lines:
+    number = pattern.match(entry).group()
+    print(number)
+
+        
 # Need to get my code to look at the first and last number and put them together. Lastly get the sum of all numbers.
