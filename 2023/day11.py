e96542046322e730138fd3fbd22047e5c9fa3693
iyoshi9 <sebastiangio@live.dk>
2023-12-20 17:45:04 +0100

    Update day11.py

--- 2023/day11.py
+++ 2023/day11.py
@@ -2,3 +2,14 @@
 
 # Part 1
 file = open('Day_11.txt', 'r')
+
+z = "#" # Amount of "#"
+x = 0
+y = 1
+# Count the "#"
+for x in z:
+    for y in z + 1: 
+         
+        y = y + 1
+x = x + 1    
+# Make it count the length of the start position to the end position 
