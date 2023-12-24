eb3017b362203a5daf06d5de13dcc61489a62abe
iyoshi9 <sebastiangio@live.dk>
2023-12-24 02:12:59 +0100

    Update day12.py

--- 2023/day12.py
+++ 2023/day12.py
@@ -2,3 +2,6 @@
 
 # Part 1
 file = open('Day_12.txt', 'r')
+# The numbers depicts the broken springs that are near each other with an operational spring between them.
+# Find the amount of possiblities for these combinations which will be the arrangments
+# Get the sum of the arrangements possibilites
